5d35d447d848d21ce0760fd549f8d0f4f9512af0
German Saprykin <german.saprykin@it-nomads.com>
2019-02-09 22:30:05 +0800

    Fix travis config.

--- Tests/SwiftCentrifugeTests/BasicTests.swift
+++ Tests/SwiftCentrifugeTests/BasicTests.swift
@@ -6,7 +6,7 @@ final class SwiftCentrifugeTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(1+1, 3)
+        XCTAssertEqual(1+1, 2)
     }
 
     static var allTests = [
