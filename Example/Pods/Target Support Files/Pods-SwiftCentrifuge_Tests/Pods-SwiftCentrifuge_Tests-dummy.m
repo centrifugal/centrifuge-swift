3be5fe61cbaef6f9207a0eb17384ff1ba168fe41
German Saprykin <german.saprykin@it-nomads.com>
2019-02-09 21:46:25 +0800

    Ignore Pods.

--- Example/Pods/Target Support Files/Pods-SwiftCentrifuge_Tests/Pods-SwiftCentrifuge_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftCentrifuge_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftCentrifuge_Tests
-@end
--- Example/Pods/Target Support Files/SwiftCentrifuge/SwiftCentrifuge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftCentrifuge : NSObject
-@end
-@implementation PodsDummy_SwiftCentrifuge
-@end
