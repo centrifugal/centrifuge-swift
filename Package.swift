35b514475207b0f99c691cdba744f3754f0738df
Danqing Liu <git@danqing.io>
2019-11-09 09:35:36 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCentrifuge",
+    products: [
+        .library(name: "SwiftCentrifuge", targets: ["SwiftCentrifuge"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream", from:"3.0.6"),
         .package(url: "https://github.com/apple/swift-protobuf", from:"1.7.0")
