196e5d793282d57b505b96aa57ae436cb339f9ba
Andrei <78256107+shalom-aviv@users.noreply.github.com>
2025-08-10 08:36:39 +0300

    public constructors for events to simplify testing (#113)

--- Sources/SwiftCentrifuge/Delegate.swift
+++ Sources/SwiftCentrifuge/Delegate.swift
@@ -9,118 +9,266 @@
 import Foundation
 
 public struct CentrifugeConnectedEvent{
-    public var client: String
-    public var data: Data?
+    public let client: String
+    public let data: Data?
+
+    public init(client: String, data: Data? = nil) {
+        self.client = client
+        self.data = data
+    }
 }
 
 public struct CentrifugeDisconnectedEvent{
-    public var code: UInt32
-    public var reason: String
+    public let code: UInt32
+    public let reason: String
+
+    public  init(code: UInt32, reason: String) {
+        self.code = code
+        self.reason = reason
+    }
 }
 
 public struct CentrifugeConnectingEvent{
-    public var code: UInt32
-    public var reason: String
+    public let code: UInt32
+    public let reason: String
+
+    public init(code: UInt32, reason: String) {
+        self.code = code
+        self.reason = reason
+    }
 }
 
-public struct CentrifugeConnectionTokenEvent {}
+public struct CentrifugeConnectionTokenEvent {
+    public init() {}
+}
 
 public struct CentrifugeJoinEvent {
-    public var client: String
-    public var user: String
-    public var connInfo: Data
-    public var chanInfo: Data
+    public let client: String
+    public let user: String
+    public let connInfo: Data
+    public let chanInfo: Data
+
+    public init(client: String, user: String, connInfo: Data, chanInfo: Data) {
+        self.client = client
+        self.user = user
+        self.connInfo = connInfo
+        self.chanInfo = chanInfo
+    }
 }
 
 public struct CentrifugeLeaveEvent {
-    public var client: String
-    public var user: String
-    public var connInfo: Data
-    public var chanInfo: Data
+    public let client: String
+    public let user: String
+    public let connInfo: Data
+    public let chanInfo: Data
+
+    public init(client: String, user: String, connInfo: Data, chanInfo: Data) {
+        self.client = client
+        self.user = user
+        self.connInfo = connInfo
+        self.chanInfo = chanInfo
+    }
 }
 
 public struct CentrifugeMessageEvent {
-    public var data: Data
+    public let data: Data
+
+    public init(data: Data) {
+        self.data = data
+    }
 }
 
 public struct CentrifugeErrorEvent {
-    public var error: Error
+    public let error: Error
+
+    public init(error: Error) {
+        self.error = error
+    }
 }
 
 public struct CentrifugePublicationEvent {
-    public var data: Data
-    public var offset: UInt64
-    public var tags: [String: String]
-    public var info: CentrifugeClientInfo?
+    public let data: Data
+    public let offset: UInt64
+    public let tags: [String: String]
+    public let info: CentrifugeClientInfo?
+
+    public init(data: Data, offset: UInt64, tags: [String : String], info: CentrifugeClientInfo? = nil) {
+        self.data = data
+        self.offset = offset
+        self.tags = tags
+        self.info = info
+    }
 }
 
 public struct CentrifugeSubscriptionTokenEvent {
-    public var channel: String
+    public let channel: String
+
+    public init(channel: String) {
+        self.channel = channel
+    }
 }
 
 public struct CentrifugeSubscriptionErrorEvent {
-    public var error: Error
+    public let error: Error
+
+    public init(error: Error) {
+        self.error = error
+    }
 }
 
 public struct CentrifugeSubscribedEvent {
-    public var wasRecovering = false
-    public var recovered = false
-    public var positioned = false
-    public var recoverable = false
-    public var streamPosition: StreamPosition? = nil
-    public var data: Data?
+    public let wasRecovering: Bool
+    public let recovered: Bool
+    public let positioned: Bool
+    public let recoverable: Bool
+    public let streamPosition: StreamPosition?
+    public let data: Data?
+
+    public init(
+        wasRecovering: Bool = false,
+        recovered: Bool = false,
+        positioned: Bool = false,
+        recoverable: Bool = false,
+        streamPosition: StreamPosition? = nil,
+        data: Data? = nil
+    ) {
+        self.wasRecovering = wasRecovering
+        self.recovered = recovered
+        self.positioned = positioned
+        self.recoverable = recoverable
+        self.streamPosition = streamPosition
+        self.data = data
+    }
 }
 
 public struct CentrifugeUnsubscribedEvent {
-    public var code: UInt32
-    public var reason: String
+    public let code: UInt32
+    public let reason: String
+
+    public init(code: UInt32, reason: String) {
+        self.code = code
+        self.reason = reason
+    }
 }
 
 public struct CentrifugeSubscribingEvent {
-    public var code: UInt32
-    public var reason: String
+    public let code: UInt32
+    public let reason: String
+
+    public init(code: UInt32, reason: String) {
+        self.code = code
+        self.reason = reason
+    }
 }
 
 public struct CentrifugeServerSubscribedEvent {
-    public var channel: String
-    public var wasRecovering = false
-    public var recovered = false
-    public var positioned = false
-    public var recoverable = false
-    public var streamPosition: StreamPosition? = nil
-    public var data: Data?
+    public let channel: String
+    public let wasRecovering: Bool
+    public let recovered: Bool
+    public let positioned: Bool
+    public let recoverable: Bool
+    public let streamPosition: StreamPosition?
+    public let data: Data?
+
+    public init(
+        channel: String,
+        wasRecovering: Bool = false,
+        recovered: Bool = false,
+        positioned: Bool = false,
+        recoverable: Bool = false,
+        streamPosition: StreamPosition? = nil,
+        data: Data? = nil
+    ) {
+        self.channel = channel
+        self.wasRecovering = wasRecovering
+        self.recovered = recovered
+        self.positioned = positioned
+        self.recoverable = recoverable
+        self.streamPosition = streamPosition
+        self.data = data
+    }
 }
 
 public struct CentrifugeServerSubscribingEvent {
-    public var channel: String
+    public let channel: String
+
+    public  init(channel: String) {
+        self.channel = channel
+    }
 }
 
 public struct CentrifugeServerUnsubscribedEvent {
-    public var channel: String
+    public let channel: String
+
+    public init(channel: String) {
+        self.channel = channel
+    }
 }
 
 public struct CentrifugeServerPublicationEvent {
-    public var channel: String
-    public var data: Data
-    public var offset: UInt64
-    public var tags: [String: String]
-    public var info: CentrifugeClientInfo?
+    public let channel: String
+    public let data: Data
+    public let offset: UInt64
+    public let tags: [String: String]
+    public let info: CentrifugeClientInfo?
+
+    public init(
+        channel: String,
+        data: Data,
+        offset: UInt64,
+        tags: [String : String],
+        info: CentrifugeClientInfo? = nil
+    ) {
+        self.channel = channel
+        self.data = data
+        self.offset = offset
+        self.tags = tags
+        self.info = info
+    }
 }
 
 public struct CentrifugeServerJoinEvent {
-    public var channel: String
-    public var client: String
-    public var user: String
-    public var connInfo: Data?
-    public var chanInfo: Data?
+    public let channel: String
+    public let client: String
+    public let user: String
+    public let connInfo: Data?
+    public let chanInfo: Data?
+
+    public init(
+        channel: String,
+        client: String,
+        user: String,
+        connInfo: Data? = nil,
+        chanInfo: Data? = nil
+    ) {
+        self.channel = channel
+        self.client = client
+        self.user = user
+        self.connInfo = connInfo
+        self.chanInfo = chanInfo
+    }
 }
 
 public struct CentrifugeServerLeaveEvent {
-    public var channel: String
-    public var client: String
-    public var user: String
-    public var connInfo: Data?
-    public var chanInfo: Data?
+    public let channel: String
+    public let client: String
+    public let user: String
+    public let connInfo: Data?
+    public let chanInfo: Data?
+
+    public init(
+        channel: String,
+        client: String,
+        user: String,
+        connInfo: Data? = nil,
+        chanInfo: Data? = nil
+    ) {
+        self.channel = channel
+        self.client = client
+        self.user = user
+        self.connInfo = connInfo
+        self.chanInfo = chanInfo
+    }
 }
 
 public protocol CentrifugeClientDelegate: AnyObject {
--- Sources/SwiftCentrifuge/Types.swift
+++ Sources/SwiftCentrifuge/Types.swift
@@ -10,47 +10,81 @@ import Foundation
 import SwiftProtobuf
 
 public struct CentrifugePublication {
-    public var offset: UInt64
-    public var data: Data
-    public var clientInfo: CentrifugeClientInfo?
+    public let offset: UInt64
+    public let data: Data
+    public let clientInfo: CentrifugeClientInfo?
+
+    public init(offset: UInt64, data: Data, clientInfo: CentrifugeClientInfo? = nil) {
+        self.offset = offset
+        self.data = data
+        self.clientInfo = clientInfo
+    }
 }
 
 public struct CentrifugeHistoryResult {
-    public var publications: [CentrifugePublication]
-    public var offset: UInt64
-    public var epoch: String
+    public let publications: [CentrifugePublication]
+    public let offset: UInt64
+    public let epoch: String
+
+    public init(publications: [CentrifugePublication], offset: UInt64, epoch: String) {
+        self.publications = publications
+        self.offset = offset
+        self.epoch = epoch
+    }
 }
 
 public struct CentrifugeClientInfo {
-    public var client: String
-    public var user: String
-    public var connInfo: Data
-    public var chanInfo: Data
+    public let client: String
+    public let user: String
+    public let connInfo: Data
+    public let chanInfo: Data
+
+    public init(client: String, user: String, connInfo: Data, chanInfo: Data) {
+        self.client = client
+        self.user = user
+        self.connInfo = connInfo
+        self.chanInfo = chanInfo
+    }
 }
 
-public struct CentrifugePublishResult {}
+public struct CentrifugePublishResult {
+    public init() {}
+}
 
 public struct CentrifugeRpcResult {
-    public var data: Data
+    public let data: Data
+
+    public init(data: Data) {
+        self.data = data
+    }
 }
 
 public struct CentrifugePresenceResult {
-    public var presence: [String: CentrifugeClientInfo]
+    public let presence: [String: CentrifugeClientInfo]
+
+    public init(presence: [String : CentrifugeClientInfo]) {
+        self.presence = presence
+    }
 }
 
 public struct CentrifugePresenceStatsResult {
-    public var numClients: UInt32
-    public var numUsers: UInt32
+    public let numClients: UInt32
+    public let numUsers: UInt32
+
+    public init(numClients: UInt32, numUsers: UInt32) {
+        self.numClients = numClients
+        self.numUsers = numUsers
+    }
 }
 
 public struct CentrifugeStreamPosition {
+    public let offset: UInt64
+    public let epoch: String
+
     public init(offset: UInt64, epoch: String) {
         self.offset = offset
         self.epoch = epoch
     }
-    
-    public var offset: UInt64
-    public var epoch: String
 }
 
 struct ServerSubscription {
